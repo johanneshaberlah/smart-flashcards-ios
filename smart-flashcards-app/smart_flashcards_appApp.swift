a328b326a6a1ddf9fba677820e6590c23775aa0c
Johannes Haberlah <haberlah.j@zdf.de>
2026-02-03 15:53:38 +0100

    Initial Commit

---
+++ smart-flashcards-app/smart_flashcards_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  smart_flashcards_appApp.swift
+//  smart-flashcards-app
+//
+//  Created by Johannes Haberlah on 03.02.26.
+//
+
+import SwiftUI
+
+@main
+struct smart_flashcards_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
